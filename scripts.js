526e4e3ae963286a08aa0b8048af2fd2110d8822
chuckgstanley <cgs2138@columbia.edu>
2014-04-14 22:26:25 -0400

    moved video link out of scripts

---
+++ scripts.js
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- saved from url=(0261)https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=817c9038f6&view=att&th=1455bc72e5018f59&attid=0.1&disp=inline&realattid=f_htyicav60&safe=1&zw&saduie=AG9B_P_q9b4G7Xi-lS6EfnZUpYi9&sadet=1397447310788&sads=eOCvJTKq4CpPUWXqMGCtXqgYC2A&sadssc=1 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style id="clearly_highlighting_css" type="text/css">/* selection */ html.clearly_highlighting_enabled ::-moz-selection { background: rgba(246, 238, 150, 0.99); } html.clearly_highlighting_enabled ::selection { background: rgba(246, 238, 150, 0.99); } /* cursor */ html.clearly_highlighting_enabled {    /* cursor and hot-spot position -- requires a default cursor, after the URL one */    cursor: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--cursor.png") 14 16, text; } /* highlight tag */ em.clearly_highlight_element {    font-style: inherit !important; font-weight: inherit !important;    background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--yellow.png");    background-repeat: repeat-x; background-position: top left; background-size: 100% 100%; } /* the delete-buttons are positioned relative to this */ em.clearly_highlight_element.clearly_highlight_first { position: relative; } /* delete buttons */ em.clearly_highlight_element a.clearly_highlight_delete_element {    display: none; cursor: pointer;    padding: 0; margin: 0; line-height: 0;    position: absolute; width: 34px; height: 34px; left: -17px; top: -17px;    background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--delete-sprite.png"); background-repeat: no-repeat; background-position: 0px 0px; } em.clearly_highlight_element a.clearly_highlight_delete_element:hover { background-position: -34px 0px; } /* retina */ @media (min--moz-device-pixel-ratio: 2), (-webkit-min-device-pixel-ratio: 2), (min-device-pixel-ratio: 2) {    em.clearly_highlight_element { background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--yellow@2x.png"); }    em.clearly_highlight_element a.clearly_highlight_delete_element { background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--delete-sprite@2x.png"); background-size: 68px 34px; } } </style><style>[touch-action="none"]{ -ms-touch-action: none; touch-action: none; }[touch-action="pan-x"]{ -ms-touch-action: pan-x; touch-action: pan-x; }[touch-action="pan-y"]{ -ms-touch-action: pan-y; touch-action: pan-y; }[touch-action="scroll"],[touch-action="pan-x pan-y"],[touch-action="pan-y pan-x"]{ -ms-touch-action: pan-x pan-y; touch-action: pan-x pan-y; }</style></head><body>
+
+	
+		
+		
+		
+		
+
+					
+	
+	
+	<div>
+<ul>
+	
+<button>
+  MORE
+</button>
+
+  <li>
+    <a href="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=817c9038f6&view=att&th=1455bc72e5018f59&attid=0.1&disp=inline&realattid=f_htyicav60&safe=1&zw&saduie=AG9B_P_q9b4G7Xi-lS6EfnZUpYi9&sadet=1397447310788&sads=eOCvJTKq4CpPUWXqMGCtXqgYC2A&sadssc=1#0.1_">
+      Dropdown <span></span>
+    </a>
+    <ul>
+    	<a href="http://www.coneyislandhistory.org/" target="_blank">Coney Island History</a>
+      
+    </ul>
+  </li>
+
+</ul>
+		<div>
+		<h1>Coney Island!</h1>
+		<div>
+
+			<div>
+				<h3>Coney Island Cyclone</h3>
+				<p> Wow!! What a fun ride. Hope you didn't have too many hotdogs.</p>		
+			
+				
+<button>
+  Wee!
+</button>
+
+
+			</div>
+
+			<div>
+
+					<img src="http://images/cyclone.jpg">
+			</div>
+
+		</div> 
+			
+				
+
+			<div>
+				<div>
+				<h3>Wonder Wheel</h3>
+				<p>This is the actual ferris wheel that Washington's troops
+					used to escape to New Jersey, following their defeat at the 
+					Battle of Brooklyn</p>	
+			</div>
+
+			<div>
+					<img src="http://images/wonder-wheel.jpg">	
+
+			</div>
+		</div>
+
+
+		<div>
+			<div>
+				<div>
+				<h3>Parachute</h3>
+				<p>The Battle was reenacted in a choreographed parachute performance
+					during the opening ceremonies of the 1985 Summer Olympics in Brooklyn</p>
+			</div>
+
+			<div>
+				<img src="http://images/parachute.jpg">
+
+			</div>
+
+		</div> 
+
+				
+<div>
+  <div>
+    <div>
+      <div>
+        <button type="button">×</button>
+        <h4>A Ride on the Cyclone</h4>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        <button type="button">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div> 
+						
+<div>
+  <div>
+    <div>
+      <div>
+        <button type="button">×</button>
+        <h4>Coney Island Home Page</h4>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        <button type="button">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div> 
+
+		
+		
+
+		
+		</div></div></div></body></html>
